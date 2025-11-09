5173ac1e1ee43313509d7b4cac516a0d9dfe36df
TaniaAlicia <targ5181@gmail.com>
2025-11-09 18:17:17 -0300

    arreglando en account.ts

--- src/types/account.ts
+++ src/types/account.ts
@@ -1,6 +1,10 @@
 export type AccountType = {
-  id: string;
-  balance: number;
-  alias: string;
-  cvu: string;
+  id: number;
+  user_id?: number;
+  balance?: number; 
+  alias?: string;
+  cvu?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  available_amount?: number;
 };
