8266e9633d5045daaaea100a0965877dd2068e2e
TaniaAlicia <targ5181@gmail.com>
2025-11-09 17:47:31 -0300

    arreglando import

--- src/types/Account.ts
+++ src/types/Account.ts
@@ -1,18 +1,11 @@
 export type AccountType = {
   id: number;
   user_id?: number;
-  balance?: number; // centavos o pesos, según tu API (aquí asumimos pesos)
+  balance?: number; 
   alias?: string;
   cvu?: string;
   createdAt?: string;
   updatedAt?: string;
   available_amount?: number;
 };
-/* export type Account = {
-  id: number;
-  user_id?: number;
-  userId?: number;
-  cvu?: string;
-  alias?: string;
-  balance?: number;
-}; */
+
